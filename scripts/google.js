8ef7bc7dd6deab334a6c2d725c76bdf87a8e9dac
Hay Kranen <huskyr@gmail.com>
2017-11-05 13:11:35 +0100

    Adding toggle cruft button to google script

--- scripts/google.js
+++ scripts/google.js
@@ -1,6 +1,20 @@
 $("body").append(`<style>
-    #tvcap,
-    .cu-container {
+    body.hidecruft #tvcap,
+    body.hidecruft .cu-container,
+    body.hidecruft #bottomads {
         display: none;
     }
+
+    [toggleCruft] {
+        text-decoration: underline !important;
+        cursor: pointer;
+    }
 </style>`);
+
+$("body").addClass('hidecruft');
+
+$("#resultStats").append('| <a toggleCruft>Toggle cruft</a>');
+
+$("#resultStats").on('click', '[toggleCruft]', () => {
+    $("body").toggleClass('hidecruft');
+});
