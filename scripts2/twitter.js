4ebc7806d2c12aa6b9828a6ded61e215737bacf0
Hay Kranen <huskyr@gmail.com>
2018-02-01 23:45:15 +0100

    adding twitter

---
+++ scripts2/twitter.js
@@ -0,0 +1,11 @@
+export default {
+    host : 'twitter.com',
+
+    css : `
+        .has-recap,
+        .js-activity-generic,
+        .promoted-tweet {
+            display: none;
+        }
+    `
+}
