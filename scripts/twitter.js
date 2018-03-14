6d3a57befcd25de4688db8812770f6b04d517016
Hay Kranen <huskyr@gmail.com>
2018-03-14 18:30:17 +0100

    Adding no rt/favs button

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,11 +1,55 @@
+import { $, $$ } from '../utils.js';
+
 export default {
     host : 'twitter.com',
 
     css : `
         .has-recap,
         .js-activity-generic,
+        .wtf-module,
         .promoted-tweet {
-            display: none;
+            display: none !important;
         }
-    `
+
+        body.nocruft [data-is-likedby],
+        body.nocruft [data-is-retweet] {
+            display: none !important;
+        }
+
+        [data-toggle-nocruft] {
+            cursor: pointer;
+        }
+    `,
+
+    js() {
+        function addTypes() {
+            // Tag 'liked by'
+            $$(".Icon--heartBadge").forEach((el) => {
+                el.parentNode.parentNode.parentNode.parentNode.setAttribute('data-is-likedby', '');
+            });
+
+            // Tag retweets
+            $$("[data-retweet-id]").forEach((el) => {
+                el.parentNode.setAttribute('data-is-retweet', '');
+            })
+        }
+
+        setInterval(addTypes, 2000);
+
+        addTypes();
+
+        $("#global-actions").innerHTML += `
+            <li>
+                <a data-toggle-nocruft>
+                    <span class="text">No RT / Favs</span>
+                </a>
+            </li>
+        `;
+
+        $("[data-toggle-nocruft]").addEventListener('click', () => {
+            $("body").classList.toggle('nocruft');
+        });
+
+        $("body").classList.add('nocruft');
+    }
 }
