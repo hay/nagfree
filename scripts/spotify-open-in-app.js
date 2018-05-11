2795ad463e3f6de711e9f333d8234589e3259c4b
Hay Kranen <huskyr@gmail.com>
2018-05-11 22:56:03 +0200

    New script for opening open.spotify.com links in the desktop app

---
+++ scripts/spotify-open-in-app.js
@@ -0,0 +1,13 @@
+// Open open.spotify.com links in the desktop app instead of with the web client
+// Based on < https://greasyfork.org/en/scripts/38920-spotify-open-in-app/code >
+
+export default {
+    host : 'spotify.com',
+
+    js() {
+        const data = document.URL.match(/[\/\&](track|playlist|album|artist)\/([^\&\#\/\?]+)/i);
+        const type = data[0];
+        const id = data[1];
+        window.location.replace(`spotify:${type}:${id}`);
+    }
+};
