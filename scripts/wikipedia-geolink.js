aa9ada4bf3697b8f548e2a6a13357124fded6b13
Hay Kranen <huskyr@gmail.com>
2018-05-22 14:38:29 +0200

    Replacing geohack links on Wikipedia pages directly to Google Maps

---
+++ scripts/wikipedia-geolink.js
@@ -0,0 +1,20 @@
+// Directly link the coordinates in articles to Google Maps instead of the
+// geohack tool
+import { $ } from '../utils.js';
+
+export default {
+    host : 'wikipedia.org',
+
+    js() {
+        const coord = $("#coordinates");
+
+        if (!coord) return;
+
+        let coordDec = coord.querySelector('.geo-nondefault .geo').innerText;
+        coordDec = coordDec.replace('; ',',');
+        const link = coord.querySelector('a.external');
+
+        link.href = `https://www.google.com/maps/?q=${coordDec}`;
+        link.target = '_blank';
+    }
+};
