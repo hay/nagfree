0bf3ab93968e642d09f45e5fc6e1e256e207abde
Hay Kranen <huskyr@gmail.com>
2018-05-23 02:09:13 +0200

    Removed old vk script, adding new one to make the fixed header static

---
+++ scripts/volkskrant-static-header.js
@@ -0,0 +1,10 @@
+// Make fixed header not fixed
+export default {
+    host : 'volkskrant.nl',
+
+    css : `
+        .app-header-default {
+            position: inherit !important;
+        }
+    `
+};
