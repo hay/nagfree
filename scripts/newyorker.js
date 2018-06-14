58d54e7f68bd84f72ae485bbf6bbb0d8d59ff72f
Hay Kranen <huskyr@gmail.com>
2018-06-14 23:39:07 +0200

    Adding reading time to newyorker

--- scripts/newyorker.js
+++ scripts/newyorker.js
@@ -1,13 +1,17 @@
 import { $, countWords, elementFromHtml, numberWithCommas } from '../utils.js';
 
 const article = $("#articleBody");
+const WORDS_PER_MINUTE = 350;
 
 export default {
     host : 'newyorker.com',
 
     js() {
-        const words = numberWithCommas(countWords(article.textContent));
-        const count = elementFromHtml(`<p><em>${words} words</em></p>`);
+        const words = countWords(article.textContent);
+        const minutes = Math.round(words / WORDS_PER_MINUTE);
+        const count = elementFromHtml(`<p>
+            <em>${numberWithCommas(words)} words, ${minutes} minutes to read</em>
+        </p>`);
         article.prepend(count);
     }
 }
