51119e8e41fa2a22e0d6006f14fe207dcd2f0322
Hay Kranen <huskyr@gmail.com>
2018-12-12 16:38:16 +0100

    Adding nytimes anti-adblocker script

---
+++ scripts/nytimes.js
@@ -0,0 +1,20 @@
+// Remove anti-adblocker messages
+import { $$ } from '../utils.js';
+
+export default {
+    host : 'nytimes.com',
+
+    js() {
+        // No obvious ways to hide the anti-adblocker stuff, so we need to do
+        // something quirky
+        $$('a[href*="allow-ads-on-nytimes-com"]').forEach((el) => {
+            el.parentElement.style.display = 'none';
+        });
+    },
+
+    css : `
+        .adblock_sub {
+            display: none !important;
+        }
+    `
+}
